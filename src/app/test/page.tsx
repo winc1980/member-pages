19460f8ca655b3eab6c85d2e273f288989dcb6a8
inumaruseiya <s.inumaru2023@gmail.com>
2024-02-26 08:08:46 +0900

    月間と年間のコミット数ランキングを実装

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,27 @@
-import { createRanking } from './githubApi';
+import { createYearlyRanking,createMonthlyRanking, createRanking } from './githubApi';
 
 export default async function Page() {
   return (
     <div>
+      <h1>年間コミット数ランキング</h1>
+      <div>
+        {(await createYearlyRanking()).map((user, index) => (
+          <div key={index}>
+            {user.author}: {user.yearlyCommits}
+          </div>
+        ))}
+      </div>
+      <h1>月間コミット数ランキング</h1>
+      <div>
+        {(await createMonthlyRanking()).map((user, index) => (
+          <div key={index}>
+            {user.author}: {user.monthlyCommits}
+          </div>
+        ))}
+      </div>
       <h1>コミット数ランキング</h1>
       <div>
-        {(await createRanking()).map((user, index) => (
+      {(await createRanking()).map((user, index) => (
           <div key={index}>
             {user.author}: {user.commits}
           </div>
