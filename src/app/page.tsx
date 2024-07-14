7558ab58f235c7445a4af972885148a91fa0be10
Rio <9rio10089@gmail.com>
2024-07-14 16:30:08 +0900

    add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 // import Image from "next/image";
 // import Link from "next/link";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
+      <Footer />
     </main>
   );
 }
