583e23bf51ed3de4d9f8c20d954ef2b1bc2c23e0
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-07-14 18:51:05 +0900

    add helper.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { auth } from "@/auth";
 import { type FC } from "react";
 
-const Home: FC = () => {
+const Home: FC = async () => {
+  const session = await auth();
   return (
     <div className="">
       <div className="text-lg font-bold">Home</div>
@@ -8,6 +10,9 @@ const Home: FC = () => {
         <span className="text-blue-500">Hello</span>
         <span className="text-red-500">World</span>
       </div>
+      <pre className="bg-slate-100 p-2 text-sm text-slate-700">
+       {JSON.stringify(session, null, 2)}
+     </pre>
     </div>
   );
 };
