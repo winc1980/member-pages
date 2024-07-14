738dd3f1d92f47b956e0a7ea84d56024f735102c
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-07-14 19:06:12 +0900

    add whoami/route.ts

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import WhoAmIServerAction from "./WhoAmIServerAction";
 
 export default async function TestRoute() {
   const session = await auth();
@@ -7,6 +8,7 @@ export default async function TestRoute() {
     <main>
       <h1 className="text-3xl mb-5">Test Route</h1>
       <div>User: {session?.user?.name}</div>
+      <WhoAmIServerAction />
     </main>
   );
 }
