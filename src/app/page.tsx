3443383a850172c2591bfc667b66dc3f2bea7de0
Rio <9rio10089@gmail.com>
2024-07-18 22:04:57 +0900

    feature: fetch profile data from spreadsheet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@
 // import Link from "next/link";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
+      <Profile />
       <Footer />
     </main>
   );
